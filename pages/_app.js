dd145722712742d98a735317cdd93869804975a3
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-02-09 18:00:31 +0100

    add meta desc

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,10 @@ class MyApp extends App {
         <Head>
           <title>Oz Ensemble</title>
           <link rel="shortcut icon" href="/images/logo_oz.png" />
+          <meta
+            name="description"
+            content="Oz Ensemble est une application mobile pour maitriser sa consommation d’alcool. Ce dispositif expérimental créé par le Dr Géraldine Talbot, médecin addictologue de l'association CaPASSCité, financé par l'ARS, la MILDECA et la DINUM, en collaboration avec la Fabrique du numérique des ministères sociaux."
+          />
         </Head>
         <div className="mt-[70px]">
           <Component {...pageProps} />
