5ae04193cf71ffeba606c8e541977f6bbcf027b5
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2023-03-08 09:25:04 +0100

    feat: download url for all stores (#74)

--- constants.js
+++ constants.js
@@ -1,3 +1,6 @@
+export const ANDROID_APP_ID = "com.addicto"
 export const ANDROID_URL =
   "https://play.google.com/store/apps/details?id=com.addicto"
+export const IOS_APP_ID = "1498190343"
 export const IOS_URL = "https://apps.apple.com/fr/app/oz-ensemble/id1498190343"
+export const ROOT_URL = "https://ozensemble.fr"
