397df1265abbea020b9330a59f6b90ca911eb56a
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-03-08 09:19:13 +0100

    feat: download url for all stores

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "tailwindcss/tailwind.css"
 import "../style/default.css"
 
 import { init } from "@socialgouv/matomo-next"
+import { ANDROID_APP_ID, IOS_APP_ID, ROOT_URL } from "constants"
 import App from "next/app"
 import Head from "next/head"
 
@@ -17,12 +18,30 @@ class MyApp extends App {
     return (
       <>
         <Head>
-          <title>Oz Ensemble</title>
+          <title key="index">Oz Ensemble</title>
           <link rel="shortcut icon" href="/images/logo_oz.png" />
+          <meta property="og:title" content="Oz Ensemble" key="index" />
           <meta
             name="description"
-            content="Oz Ensemble est une application mobile pour maitriser sa consommation d’alcool. Ce dispositif expérimental créé par le Dr Géraldine Talbot, médecin addictologue de l'association CaPASSCité, financé par l'ARS, la MILDECA et la DINUM, en collaboration avec la Fabrique du numérique des ministères sociaux."
+            content="Oz Ensemble est une application mobile pour maitriser sa consommation d'alcool. Ce dispositif expérimental créé par le Dr Géraldine Talbot, médecin addictologue de l'association CaPASSCité, financé par l'ARS, la MILDECA et la DINUM, en collaboration avec la Fabrique du numérique des ministères sociaux."
+            key="index"
           />
+          <meta
+            property="og:description"
+            content="Oz Ensemble est une application mobile pour maitriser sa consommation d'alcool. Ce dispositif expérimental créé par le Dr Géraldine Talbot, médecin addictologue de l'association CaPASSCité, financé par l'ARS, la MILDECA et la DINUM, en collaboration avec la Fabrique du numérique des ministères sociaux."
+            key="index"
+          />
+          <meta charset="UTF-8" />
+          <title key="download">Télécharger OzEnsemble</title>
+          <meta property="og:url" content={`${ROOT_URL}/download`} />
+          <meta property="og:image" content="/images/logo_oz.png" />
+          <meta property="og:type" content="article" />
+          <meta property="al:ios:app_store_id" content={IOS_APP_ID} />
+          <meta property="al:ios:app_name" content="Oz Ensemble" />
+          <meta property="al:android:app_name" content="Oz Ensemble" />
+          <meta property="al:android:package" content={ANDROID_APP_ID} />
+          <meta property="al:web:url" content={ROOT_URL} />
+          <meta property="al:web:should_fallback" content="false" />
         </Head>
         <div className="mt-[70px]">
           <Component {...pageProps} />
---
+++ pages/download.js
@@ -0,0 +1,78 @@
+import Head from "next/head"
+import Script from "next/script"
+
+import {
+  ANDROID_APP_ID,
+  ANDROID_URL,
+  IOS_APP_ID,
+  IOS_URL,
+  ROOT_URL,
+} from "../constants"
+
+// ANDROID_URL
+// IOS_URL
+
+const download = () => {
+  return (
+    <Head>
+      <meta charset="UTF-8" />
+      <title>Télécharger OzEnsemble</title>
+      <meta
+        property="og:title"
+        content="Télécharger OzEnsemble"
+        key="download"
+      />
+      <meta
+        name="description"
+        content="Télécharger Oz Ensemble, l'une application mobile pour maitriser sa consommation d'alcool."
+        key="download"
+      />
+      <meta
+        property="og:description"
+        content="Télécharger Oz Ensemble, l'une application mobile pour maitriser sa consommation d'alcool."
+        key="download"
+      />
+      <meta property="og:image" content="/images/logo_oz.png" />
+      <meta property="og:type" content="article" />
+      {/* <meta property="fb:app_id" content="{FACEBOOK_APP_ID}" /> */}
+      {/* <meta property="al:ios:url" content="myapp://element/{ELEMENT_ID}" /> */}
+      <meta property="al:ios:app_store_id" content={IOS_APP_ID} />
+      <meta property="al:ios:app_name" content="Oz Ensemble" />
+      {/* <meta property="al:android:url" content="myapp://element/{ELEMENT_ID}" /> */}
+      <meta property="al:android:app_name" content="Oz Ensemble" />
+      <meta property="al:android:package" content={ANDROID_APP_ID} />
+      <meta property="al:web:url" content={ROOT_URL} />
+      <meta property="al:web:should_fallback" content="false" />
+      <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+  function redirect() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var ios = /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+    if (ios) {
+      // window.location = myapp://element/{ELEMENT_ID};
+      window.setTimeout(() => {
+        window.location.replace('${IOS_URL}');
+      }, 25)
+      return
+    }
+    var android = /android/i.test(userAgent);
+    if (android) {
+      // window.location = myapp://element/{ELEMENT_ID};
+      window.setTimeout(() => {
+        window.location.replace('${ANDROID_URL}');
+      }, 25)
+      return
+    }
+    window.location.replace('${ROOT_URL}')
+  }
+  redirect()
+`,
+        }}
+      />
+    </Head>
+  )
+}
+
+export default download
