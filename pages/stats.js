4c4a5ba5a939c769dcd25c670907b14d2be230fc
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-07 12:00:29 +0100

    fix: title stats

--- pages/stats.js
+++ pages/stats.js
@@ -1,30 +1,36 @@
 import Footer from "../components/Footer"
 import Navigation from "../components/Navigation"
+import Head from "next/head"
 
 const Stats = () => {
   return (
-    <div>
-      <Navigation />
+    <>
+      <Head>
+        <title>Statistiques | Oz Ensemble</title>
+      </Head>
       <div>
-        <iframe
-          src="https://matomo-metabase-ozensemble.fabrique.social.gouv.fr/public/dashboard/217417aa-20f3-4dae-acd8-7230ba3c8862"
-          title="Oz Ensemble, statistiques"
-          width="90%"
-          height="700"
-          style={{ display: "block", margin: "0 auto", padding: "2rem 0" }}
-          // eslint-disable-next-line
-          allowTransparency
-        />
+        <Navigation />
+        <div>
+          <iframe
+            src="https://matomo-metabase-ozensemble.fabrique.social.gouv.fr/public/dashboard/217417aa-20f3-4dae-acd8-7230ba3c8862"
+            title="Oz Ensemble, statistiques"
+            width="90%"
+            height="700"
+            style={{ display: "block", margin: "0 auto", padding: "2rem 0" }}
+            // eslint-disable-next-line
+            allowTransparency
+          />
+        </div>
+        <div>
+          <p className="text-center">
+            L'affichage des données auxquelles vous avez accès est géré par
+            Métabase. L'accessibilité non-conforme de cet affichage est sous le
+            contrôle de Métabase.
+          </p>
+        </div>
+        <Footer />
       </div>
-      <div>
-        <p className="text-center">
-          L'affichage des données auxquelles vous avez accès est géré par
-          Métabase. L'accessibilité non-conforme de cet affichage est sous le
-          contrôle de Métabase.
-        </p>
-      </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
