301838b0ef75cc317662dc1d09f1f3484c8b8eb3
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-01-05 12:10:13 +0100

    fix: change modal to direct store buttons

--- pages/blog/[blog].js
+++ pages/blog/[blog].js
@@ -1,8 +1,5 @@
 import React, { useState } from "react"
-import Navigation, {
-  DownloadPopup,
-  DownloadPopupTrigger,
-} from "../../components/Navigation"
+import Navigation, { DownloadPopup } from "../../components/Navigation"
 import Footer from "../../components/Footer"
 import { serialize } from "next-mdx-remote/serialize"
 import { MDXRemote } from "next-mdx-remote"
@@ -12,13 +9,30 @@ import path from "path"
 import matter from "gray-matter"
 import { HiChevronLeft } from "react-icons/hi"
 import styles from "../../style/bloga.module.css"
+import { ANDROID_URL, IOS_URL } from "../../constants"
+import appStorePic from "../../public/images/other/app-store-fr.png"
+import googlePlayPic from "../../public/images/other/google-play-fr.png"
 
 const Blog = ({ mdxSource, data }) => {
   const [showPopup, setShowPopup] = useState(false)
+
   const components = {
-    CenteredDownloadPopupTrigger: () => (
-      <div className="flex justify-center mb-10">
-        <DownloadPopupTrigger setShowPopup={setShowPopup} />
+    DownloadButtons: () => (
+      <div className="mb-1 grid max-w-[200px] sm:max-w-[400px] sm:grid-flow-col gap-6 auto-cols-fr md:w-5/6 m-auto">
+        <a href={ANDROID_URL}>
+          <img
+            className="object-contain w-full"
+            src={googlePlayPic.src}
+            alt="télécharger dans Google Play"
+          />
+        </a>
+        <a href={IOS_URL}>
+          <img
+            className="object-contain w-full"
+            src={appStorePic.src}
+            alt="télécharger dans l'App Store"
+          />
+        </a>
       </div>
     ),
   }
