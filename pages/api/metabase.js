168f548ba3d36b28ec70abc8eb165c111aa94d2e
YoanRos <yoan.roszak@etu.imt-nord-europe.fr>
2024-03-05 16:18:44 +0100

    fix: better version

--- pages/api/metabase.js
+++ pages/api/metabase.js
@@ -3,33 +3,39 @@ import { DataFrame } from "data-forge"
 
 export default async function handler(req, res) {
   try {
-    let response = await got.post("http://localhost:3000/api/session", {
-      json: { username: "yoan.roszak@selego.co", password: "#Ep4-R6n3" },
-      responseType: "json",
-    })
+    const questionIndex = req.query.question
+    let response = await got.post(
+      "https://metabase-ozensemble.fabrique.social.gouv.fr/api/session",
+      {
+        json: { username: "yoan.roszak@selego.co", password: "***REMOVED***" },
+        responseType: "json",
+      }
+    )
     let session_id = response.body.id
     let headers = { "X-Metabase-Session": session_id }
 
-    response = await got.get("http://localhost:3000/api/card", {
-      responseType: "json",
-      headers: headers,
-    })
-
-    // filter for public questions
-    let questions = response.body.filter((q) => q.public_uuid)
-    console.log(
-      `${questions.length} public of ${response.body.length} questions`
-    )
-    const uuid = questions[0].public_uuid
     response = await got.get(
-      `http://localhost:3000/api/public/card/${uuid}/query`,
+      "https://metabase-ozensemble.fabrique.social.gouv.fr/api/card",
       {
         responseType: "json",
         headers: headers,
       }
     )
+
+    let questions = response.body.filter((q) => q.public_uuid)
+
+    const uuid = questions[questionIndex].public_uuid
+    response = await got.get(
+      `https://metabase-ozensemble.fabrique.social.gouv.fr/api/public/card/${uuid}/query`,
+      {
+        responseType: "json",
+        headers: headers,
+      }
+    )
+
     const rows = response.body.data.rows
     const df = new DataFrame(rows)
+
     res.status(200).json({ result: df.toJSON() })
   } catch (error) {
     console.error(error)
--- pages/stats.js
+++ pages/stats.js
@@ -2,19 +2,27 @@ import Footer from "../components/Footer"
 import Navigation from "../components/Navigation"
 import Head from "next/head"
 import { useState, useEffect } from "react"
-import "@gouvfr/dsfr/dist/dsfr.min.css"
-import "@gouvfr/dsfr-chart/LineChart/line-chart.common"
+import Chart from "chart.js/auto"
 
 const Stats = () => {
-  const [result, setResult] = useState(null)
   const [error, setError] = useState(null)
+  const [result, setResult] = useState(null)
+  const [result2, setResult2] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/metabase")
-        const data = await response.json()
-        setResult(data.result)
+        const response = await fetch("/api/metabase?question=0")
+        const responseData = await response.json()
+        const data = JSON.parse(responseData.result)
+
+        setResult(data)
+
+        const response2 = await fetch("/api/metabase?question=1")
+        const responseData2 = await response2.json()
+        const data2 = JSON.parse(responseData2.result)
+
+        setResult2(data2)
       } catch (error) {
         console.error(error)
         setError("Error loading data")
@@ -24,8 +32,64 @@ const Stats = () => {
     fetchData()
   }, [])
 
+  const createChart = (data, id) => {
+    const ctx = document.getElementById(id)
+
+    let parsedData
+    if (id === "myChart2") {
+      parsedData = data.map((row) => {
+        return {
+          x: new Date(row[1]).toLocaleString("en-FR", {
+            month: "long",
+            year: "numeric",
+          }),
+          y: row[0],
+        }
+      })
+    } else {
+      parsedData = data.map((row) => {
+        return {
+          x: new Date(row[0]).toLocaleString("en-FR", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          }),
+          y: row[1],
+        }
+      })
+    }
+
+    if (ctx && !Chart.getChart(ctx)) {
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "Your Data",
+              data: parsedData,
+              borderColor: "rgba(75,192,192,1)",
+              borderWidth: 2,
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            x: {
+              position: "bottom",
+            },
+            y: {
+              beginAtZero: true,
+              ticks: {
+                tickLength: 4,
+              },
+            },
+          },
+        },
+      })
+    }
+  }
   if (error) return <div>{error}</div>
-  if (result === null) return <div>Loading...</div>
 
   return (
     <>
@@ -36,20 +100,38 @@ const Stats = () => {
         <Navigation />
         <div></div>
         <div>
-          <h1>Statistiques</h1>
-          <div>
-            <h1>My DSFR LineChart</h1>
-            <LineChart
-              title="My LineChart"
-              data={result}
-              options={{
-                scales: {
-                  y: {
-                    beginAtZero: true,
-                  },
-                },
-              }}
-            />
+          <h3 className="mb-16 text-4xl xl:text-5xl font-bold text-center lg:text-4xl text-oz-blue">
+            Statistiques
+          </h3>
+          <div className="p-10 h-[1200px] lg:pt-10 sm:rounded-xl md:rounded-3xl bg-oz-gray sm:mx-20 xl:mx-auto xl:w-[1100px] mb-10">
+            <div className="flex flex-col gap-8 mb-10 text-center">
+              <div className=" bg-white rounded-2xl">
+                <div className="text-2xl font-bold">DAU</div>
+                {result ? (
+                  <canvas
+                    aria-label="DAU graphique"
+                    role="graphique"
+                    id="myChart"
+                    ref={(el) => el && createChart(result, "myChart")}
+                  ></canvas>
+                ) : (
+                  <div>Loading...</div>
+                )}
+              </div>
+              <div className=" bg-white rounded-2xl">
+                <div className="text-2xl font-bold">Engagés</div>
+                {result2 ? (
+                  <canvas
+                    aria-label="Engagés graphique"
+                    role="graphique"
+                    id="myChart2"
+                    ref={(el) => el && createChart(result2, "myChart2")}
+                  ></canvas>
+                ) : (
+                  <div>Loading...</div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
         <Footer />
