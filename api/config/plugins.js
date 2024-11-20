9f7ef0af204ee1c3aa4b58f4bd93e710f7e7a946
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-11-20 11:06:48 +0100

    feat: add s3 plugin config

--- api/config/plugins.js
+++ api/config/plugins.js
@@ -1,14 +1,35 @@
 module.exports = () => ({
-    'sitemap': {
-        enabled: true,
-        config: {
-          cron: '0 0 0 * * *',
-          limit: 45000,
-          xsl: true,
-          autoGenerate: false,
-          caching: true,
-          allowedFields: ['id', 'uid'],
-          excludedTypes: [],
+  'sitemap': {
+      enabled: true,
+      config: {
+        cron: '0 0 0 * * *',
+        limit: 45000,
+        xsl: true,
+        autoGenerate: false,
+        caching: true,
+        allowedFields: ['id', 'uid'],
+        excludedTypes: [],
+      },
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('bucket_access_key'),
+          secretAccessKey: env('bucket_secret_key'),
+          region: env('bucket_region'),
+          endpoint: env('bucket_endpoint'),
+          params: {
+            ACL: 'public-read',
+            signedUrlExpires: 15 * 60,
+            Bucket: env('ozensemble-dev-app'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
         },
       },
+    },
 });
