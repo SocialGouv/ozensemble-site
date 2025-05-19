0a53f9208faf89c988d612504564fc3c247fe0d1
Igor R <igor.renquin@sg.social.gouv.fr>
2025-05-19 17:21:15 +0200

    Update margin-top to 220px in all files

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     <>
       <header className="oz-header-gradient">
         <Navigation />
-        <div className="mt-[160px] md:flex pt-20 pb-8 md:pb-32 auto-cols-fr md:w-5/6 xl:w-[1100px] mx-[10%] md:m-auto">
+        <div className="mt-[220px] md:flex pt-20 pb-8 md:pb-32 auto-cols-fr md:w-5/6 xl:w-[1100px] mx-[10%] md:m-auto">
           <div className="w-full md:w-1/2 text-center md:text-left flex items-center">
             <div>
               <h2 className="mb-5 text-4xl font-bold">
